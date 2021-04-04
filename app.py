5b74b637bd72d3a5bba702b05f9795419c73003b
Alice Allafort <allafort.alice@gmail.com>
2021-04-03 20:03:30 -0700

    Add bokeh plot

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import pandas as pd
+import math
 
 from dotenv import load_dotenv
-load_dotenv()
 import os
-key=os.environ.get("API_KEY")
+
+from bokeh.io import output_file
+from bokeh.plotting import figure
+from bokeh.layouts import row,column
+
+from bokeh.models import ColumnDataSource
+from bokeh.models import HoverTool
+from bokeh.embed import components
+from bokeh.layouts import gridplot
+
 
 app = Flask(__name__)
 app.vars={}
@@ -17,8 +26,9 @@ def index():
     else:
         # request was a POST
         app.vars['stock_name'] = request.form['name_stock']
-        min_date,max_date=get_data(app.vars['stock_name'])
-        return render_template('end.html',stock_id=app.vars['stock_name'],min_date = min_date,max_date = max_date)
+        
+        script, div = create_bokeh(app.vars['stock_name'])
+        return render_template("show_bokeh.html", div=div,script=script)
     
 
 @app.route('/about')
@@ -27,20 +37,52 @@ def about():
 
 
 def get_data(ticker):
-    key=os.environ.get("API_KEY")
+    try:    key=os.environ.get("API_KEY")
+    except: 
+        load_dotenv()
+        key=os.environ.get("API_KEY")
+        
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
     response = requests.get(url)
     response = response.json()
-    df=pd.DataFrame(response['Time Series (Daily)']).transpose()
-
+    df=pd.DataFrame(response['Time Series (Daily)']).transpose().astype(float)
     #df.columns=[c for df.columns] Change column names
-
     df['Date']=pd.to_datetime(df.index)
-    min_date,max_date=str(min(df.Date)),str(max(df.Date))
-    return min_date,max_date
+    return df
+
+
+def create_bokeh(ticker):
+   
+    df=get_data(ticker)
+    #source = ColumnDataSource(df)
+    
+    plot_options = dict(x_axis_type="datetime",plot_width=900, plot_height=400)
+
+
+    title='Open and close %s'%ticker
+    p = figure(title=title,**plot_options)
+    
+    p.xaxis.axis_label = "Date"
+    p.yaxis.axis_label = "Price ($)"
+    p.xaxis.major_label_orientation = math.pi/4
+
+    p.line(df['Date'],df['1. open'])#, legend_label='Opening price')
+    p.line(df['Date'],df['4. close'],color="firebrick")#, legend_label='Closing price')
+
+    
+    p2 = figure(title='Day change',**plot_options)
+    p2.xaxis.axis_label = "Date"
+    p2.yaxis.axis_label = "Price ($)"
+    p2.xaxis.major_label_orientation = math.pi/4
+
+    p2.line(df['Date'],(df['4. close']-df['1. open']),color="olive")
+
+    fig = gridplot([[p],[p2]])
+
+    script, div = components(fig)
+    return script, div
 
 
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
